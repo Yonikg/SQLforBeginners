4d8e2e2ff3147d777f24616b6cba9e89686dadcc
yonikg <toyonasg@gmail.com>
2025-05-22 16:34:00 -0400

    Select queries

---
+++ select-queries.sql
@@ -0,0 +1,17 @@
+/*
+What is SQL?
+SQL (Structured Query Language) is a standard language used to communicate with databases. It allows us to store, retrieve, update, and delete data from table.
+
+What is a SELECT Statement?
+The SELECT statement is the most commonly used SQL command.
+It is used to fetch data from one or more tables in a database.
+
+*/
+
+-- To select all data from a table use the following command
+Select * from dbo.sales
+
+/*
+This will retreive all data from tables
+* represents all columns inside sales table.
+/*
